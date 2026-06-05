97c21127c3a950eaec49ac62e9184d172343fb40
Samiahaj <hajjisamia10@gmail.com>
2026-06-05 21:33:37 +0200

    layout verbeteren met nieuwe sidebar en navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,7 @@
-<div class="bg-white h-20 border-b flex justify-end items-center px-8">
+<div class="bg-white h-20 border-b flex justify-end items-center px-8 gap-6 shadow-md">
 
-    <a href="{{ route('profile.edit') }}" class="flex items-center gap-4 p-2 rounded-xl hover:bg-gray-50 transition-colors duration-150 cursor-pointer group">
+    <a href="{{ route('profile.edit') }}"
+       class="flex items-center gap-4 p-2 rounded-xl hover:bg-gray-50 transition">
 
         <div
             class="w-12 h-12 rounded-full
@@ -19,16 +20,36 @@
 
         <div>
 
-            <p class="font-semibold text-gray-700 leading-tight">
+            <p class="font-semibold text-gray-700">
                 {{ Auth::user()->name }}
             </p>
 
-            <p class="text-xs text-gray-400 group-hover:text-[#1E6BA8] transition-colors capitalize">
-                {{ Auth::user()->role }} • <span class="underline">Wijzigen</span>
+            <p class="text-xs text-gray-400 capitalize">
+                {{ Auth::user()->role }}
             </p>
 
         </div>
 
     </a>
+
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+
+        <button
+            type="submit"
+            class="bg-[#0F4C81]
+                   hover:bg-[#1E6BA8]
+                   text-white
+                   font-medium
+                   px-5 py-2
+                   rounded-xl
+                   shadow-md
+                   transition">
+
+            Uitloggen
+
+        </button>
+
+    </form>
 
 </div>
