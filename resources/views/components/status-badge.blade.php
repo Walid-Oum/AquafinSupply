7e9be0b126415c36114d05639967140edab2c326
Samiahaj <hajjisamia10@gmail.com>
2026-06-08 14:18:05 +0200

    bestelling pagina voor magazijnmedewerker

--- resources/views/components/status-badge.blade.php
+++ resources/views/components/status-badge.blade.php
@@ -2,19 +2,21 @@
 
 @php
 
-$color = match($status){
+
+
+    $color = match($status){
 
     'Nieuw' => 'bg-blue-100 text-blue-700',
 
     'In voorbereiding' => 'bg-yellow-100 text-yellow-700',
 
-    'Verzonden' => 'bg-green-100 text-green-700',
+    'Klaar om af te halen' => 'bg-green-100 text-green-700',
 
-    'Open' => 'bg-red-100 text-red-700',
+    'Afgehaald' => 'bg-gray-100 text-gray-700',
 
     default => 'bg-gray-100 text-gray-700'
 
-};
+}; 
 
 @endphp
 
