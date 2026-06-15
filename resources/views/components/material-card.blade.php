fbce9ce09dd491029aa11185f87efc2a2a3bb711
Samiahaj <hajjisamia10@gmail.com>
2026-06-15 14:58:49 +0200

    afbeeldingen toevoegen

--- resources/views/components/material-card.blade.php
+++ resources/views/components/material-card.blade.php
@@ -2,20 +2,25 @@
     $localStock = $material->stocks->first();
     $stock = $localStock?->stock ?? 0;
     $minimumStock = $localStock?->minimum_stock ?? 0;
-@endphp
 
+    $categoryImages = [
+        'Aquafin tools' => 'aquafintools.png',
+        'Bevestigingsmateriaal' => 'bevestigingsmateriaal.png',
+        'Gereedschap' => 'gereedschap.png',
+        'PBM' => 'PBM.png',
+        'Technisch onderhoud' => 'technischeonderhoud.png',
+        'Verbruiksgoederen' => 'verbruiksgoederen.png',
+    ];
+@endphp
 @if($compact)
     <a
         href="{{ route('technician.materials.show', $material->id) }}"
         class="border rounded-lg p-4 bg-white hover:shadow block">
 
-        @if($material->image)
-            <img
-                src="{{ Storage::url($material->image) }}"
-                class="w-32 h-32 object-cover rounded mb-3 mx-auto">
-        @else
-            <div class="w-32 h-32 bg-gray-100 rounded mb-3 mx-auto"></div>
-        @endif
+        <img
+    src="{{ asset('images/' . ($categoryImages[$material->category] ?? 'sidebar-bg.jpg')) }}"
+    class="w-24 h-24 md:w-32 md:h-32 object-cover rounded mb-3 mx-auto"
+    alt="{{ $material->category }}">
 
       <h3 class="text-xl font-bold min-h-[64px]">
     {{ $material->name }}
@@ -46,23 +51,18 @@
         </form>
     </a>
 @else
-    <a
+  <a
         href="{{ route('technician.materials.show', $material->id) }}"
         class="bg-white rounded-2xl shadow-md hover:shadow-xl transition overflow-hidden block">
 
-        @if($material->image)
-            <img
-                src="{{ Storage::url($material->image) }}"
-                class="w-full h-48 object-cover">
-        @else
-            <div class="w-full h-48 bg-gray-100 flex items-center justify-center">
-                <span class="text-gray-400">
-                    Geen afbeelding
-                </span>
-            </div>
-        @endif
+        <img
+            src="{{ asset('images/' . ($categoryImages[$material->category] ?? 'sidebar-bg.jpg')) }}"
+            class="w-full h-48 object-cover"
+            alt="{{ $material->category }}">
 
         <div class="p-5 flex flex-col min-h-[260px]">
+
+
             <p class="text-xs text-gray-400 uppercase mb-1">
                 {{ $material->category }}
             </p>
