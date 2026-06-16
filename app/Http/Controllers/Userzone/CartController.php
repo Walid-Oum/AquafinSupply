3023976eb2927ccb3031e6bb356736219fec989b
Vippy11-team <Walouma@hotmail.com>
2026-06-16 09:32:51 +0200

    Fix bug: missing bracket to function

--- app/Http/Controllers/Userzone/CartController.php
+++ app/Http/Controllers/Userzone/CartController.php
@@ -18,14 +18,14 @@ class CartController extends Controller
         $material = Material::findOrFail($id);
         if (!$material->is_active) {
 
-    return redirect()
-        ->back()
-        ->with(
-            'error',
-            'Dit materiaal is niet meer beschikbaar.'
-        );
+            return redirect()
+                ->back()
+                ->with(
+                    'error',
+                    'Dit materiaal is niet meer beschikbaar.'
+                );
 
-}
+        }
 
         $cart = session()->get('cart', []);
 
@@ -35,65 +35,65 @@ class CartController extends Controller
 
         } else {
 
-           $cart[$id] = [
-    'id' => $material->id,
-    'name' => $material->name,
-    'category' => $material->category,
-    'stock' => $material->stock,
-    'quantity' => 1,
-];
+            $cart[$id] = [
+                'id' => $material->id,
+                'name' => $material->name,
+                'category' => $material->category,
+                'stock' => $material->stock,
+                'quantity' => 1,
+            ];
         }
 
         session()->put('cart', $cart);
-
-       return redirect()->back()
-    ->withFragment('materials')
-    ->with('success', 'Materiaal toegevoegd aan winkelmandje');
-  
-
-    public function update(Request $request, $id)
-{
-    $request->validate([
-        'quantity' => 'required|integer|min:1',
-    ]);
-
-    $cart = session()->get('cart', []);
-
-    if (isset($cart[$id])) {
- if($request->quantity > $cart[$id]['stock']){
-
-    return redirect()
-        ->back()
-        ->with(
-            'error',
-            'Onvoldoende voorraad.'
-        );
-
-}
-        $cart[$id]['quantity'] = $request->quantity;
-
-        session()->put('cart', $cart);
-    }
-
-    return redirect()
-        ->route('cart.index')
-        ->with('success', 'Aantal bijgewerkt.');
-}
-
-
-
-    public function remove($id)
-    {
-        $cart = session()->get('cart', []);
-
-        if (isset($cart[$id])) {
-
-            unset($cart[$id]);
-
-            session()->put('cart', $cart);
-        }
 
         return redirect()->back()
-            ->with('success', 'Materiaal verwijderd');
+            ->withFragment('materials')
+            ->with('success', 'Materiaal toegevoegd aan winkelmandje');}
+
+
+        public function update(Request $request, $id)
+        {
+            $request->validate([
+                'quantity' => 'required|integer|min:1',
+            ]);
+
+            $cart = session()->get('cart', []);
+
+            if (isset($cart[$id])) {
+                if ($request->quantity > $cart[$id]['stock']) {
+
+                    return redirect()
+                        ->back()
+                        ->with(
+                            'error',
+                            'Onvoldoende voorraad.'
+                        );
+
+                }
+                $cart[$id]['quantity'] = $request->quantity;
+
+                session()->put('cart', $cart);
+            }
+
+            return redirect()
+                ->route('cart.index')
+                ->with('success', 'Aantal bijgewerkt.');
+        }
+
+
+        public
+        function remove($id)
+        {
+            $cart = session()->get('cart', []);
+
+            if (isset($cart[$id])) {
+
+                unset($cart[$id]);
+
+                session()->put('cart', $cart);
+            }
+
+            return redirect()->back()
+                ->with('success', 'Materiaal verwijderd');
+        }
     }
-}
