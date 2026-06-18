98c5964d672947ecec319c18dbb2a3b5653f0142
chayma <chayma.abdessadak@student.ehb.be>
2026-06-18 11:57:07 +0200

    Documentatie toegevoegd voor UserNotification model

--- app/Models/UserNotification.php
+++ app/Models/UserNotification.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * UserNotification Model
+ * 
+ * Vertegenwoordigt een notificatie voor een gebruiker.
+ * Notificaties worden gebruikt om gebruikers te informeren over
+ * statuswijzigingen van bestellingen, tickets of andere gebeurtenissen.
+ *
+ * @author 
+ * @version 1.0
+ */
 class UserNotification extends Model
 {
+    /**
+     * Velden die massaal mogen worden ingevuld.
+     */
     protected $fillable = [
         'user_id',
         'title',
@@ -14,11 +27,19 @@ class UserNotification extends Model
         'read_at',
     ];
 
+    /**
+     * Relatie: deze notificatie hoort bij een gebruiker.
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Accessor: controleert of de notificatie gelezen is.
+     *
+     * @return bool
+     */
     public function getIsReadAttribute(): bool
     {
         return $this->read_at !== null;
