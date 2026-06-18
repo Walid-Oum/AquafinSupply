b03c5b5f2d10ec269cf193c1f63fb20e5df5dc68
chayma <chayma.abdessadak@student.ehb.be>
2026-06-18 12:28:01 +0200

    Documentatie toegevoegd voor status-badge component

--- resources/views/components/status-badge.blade.php
+++ resources/views/components/status-badge.blade.php
@@ -1,17 +1,19 @@
+{{--
+    STATUS BADGE COMPONENT
+    Toont een status badge met kleur op basis van de status.
+    @author 
+    @version 1.0
+--}}
+
 @props(['status'])
 
 @php
     $classes = match ($status) {
-        'Nieuw' => 'bg-blue-100 text-blue-700',
+        'Nieuw', 'In behandeling' => 'bg-blue-100 text-blue-700',
         'In voorbereiding' => 'bg-yellow-100 text-yellow-700',
-        'Klaar om af te halen' => 'bg-green-100 text-green-700',
+        'Klaar om af te halen', 'Opgelost' => 'bg-green-100 text-green-700',
         'Afgehaald' => 'bg-gray-100 text-gray-700',
-        'Geannuleerd' => 'bg-red-100 text-red-700',
-
-        'Open' => 'bg-red-100 text-red-700',
-        'In behandeling' => 'bg-blue-100 text-blue-700',
-        'Opgelost' => 'bg-green-100 text-green-700',
-
+        'Geannuleerd', 'Open' => 'bg-red-100 text-red-700',
         default => 'bg-gray-100 text-gray-700',
     };
 @endphp
