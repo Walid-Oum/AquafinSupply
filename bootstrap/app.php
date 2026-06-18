f54cead3d10ce285b4f18e53981b52401198eaff
chayma <chayma.abdessadak@student.ehb.be>
2026-06-18 12:08:30 +0200

    Documentatie toegevoegd voor bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,17 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+/**
+ * Laravel Application Bootstrapper
+ * 
+ * Configureert de Laravel applicatie met routing, middleware en exception handling.
+ * Registreert custom middleware: 'role' voor rolgebaseerde toegang en
+ * 'password.changed' voor wachtwoordwijzigingscontrole.
+ *
+ * @author 
+ * @version 1.0
+ */
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,11 +22,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Registreer 'role' middleware voor rolgebaseerde autorisatie
         $middleware->alias([
             'role' => App\Http\Middleware\RoleMiddleware::class,
         ]);
     })
     ->withMiddleware(function (Middleware $middleware) {
+        // Registreer 'password.changed' middleware om te controleren of gebruiker wachtwoord heeft gewijzigd
         $middleware->alias([
             'password.changed' => \App\Http\Middleware\EnsurePasswordIsChanged::class,
         ]);
