ecbc7a6dc7d1eadd56a28d41c564095cf561c140
chayma <chayma.abdessadak@student.ehb.be>
2026-06-18 12:33:28 +0200

    Documentatie toegevoegd voor card component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,10 @@
+{{--
+    CARD COMPONENT
+    Witte container met afgeronde hoeken en schaduw.
+    @author
+    @version 1.0
+--}}
+
 <div class="bg-white rounded-xl shadow-sm p-6">
 
     {{ $slot }}
