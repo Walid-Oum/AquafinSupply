4a9726311ae42ccd390df56dbc3a440c6ea63541
Romain Musungayi <romain.musungayi@student.ehb.be>
2026-06-18 21:48:05 +0200

    Documentatie toegevoegd aan password change view

--- resources/views/auth/change-password.blade.php
+++ resources/views/auth/change-password.blade.php
@@ -1,3 +1,26 @@
+{{--
+    Pagina: Wachtwoord wijzigen
+
+    Doel:
+    Verplicht scherm waarmee een gebruiker
+    een nieuw wachtwoord moet instellen bij
+    de eerste aanmelding.
+
+    Functionaliteiten:
+    - Nieuw wachtwoord instellen
+    - Bevestiging van wachtwoord
+    - Validatie van wachtwoordregels
+
+    Gebruikersrol:
+    - Admin
+    - Technieker
+    - Magazijn
+
+    Opmerking:
+    Deze pagina wordt gebruikt wanneer een
+    gebruiker verplicht is zijn tijdelijk
+    wachtwoord te vervangen.
+--}}
 <x-app-layout>
     <div class="mx-auto max-w-xl p-8">
         <div class="rounded-xl bg-white p-6 shadow">
@@ -8,11 +31,11 @@
             <p class="mt-2 text-gray-600">
                 Om veiligheidsredenen moet je bij je eerste aanmelding een nieuw wachtwoord kiezen.
             </p>
-
+            {{-- Formulier voor het instellen van een nieuw wachtwoord --}}
             <form method="POST" action="{{ route('password.change.update') }}" class="mt-6 space-y-4">
                 @csrf
                 @method('PATCH')
-
+                {{-- Invoer van het nieuwe wachtwoord --}}
                 <div>
                     <label class="block text-sm font-semibold text-gray-700">
                         Nieuw wachtwoord
@@ -31,7 +54,7 @@
                     </p>
                     @enderror
                 </div>
-
+                {{-- Bevestiging van het nieuwe wachtwoord --}}
                 <div>
                     <label class="block text-sm font-semibold text-gray-700">
                         Bevestig nieuw wachtwoord
