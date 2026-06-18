1fef9b5ad79fd6ef1729fc04189e01cd623b7d66
chayma <chayma.abdessadak@student.ehb.be>
2026-06-18 11:48:29 +0200

    Documentatie toegevoegd voor Location model

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Location Model
+ * 
+ * Vertegenwoordigt een depot/vestiging. Gebruikers en voorraden zijn gekoppeld aan een locatie.
+ *
+ * @author 
+ * @version 1.0
+ */
 class Location extends Model
 {
+    /**
+     * Velden die massaal mogen worden ingevuld.
+     */
     protected $fillable = [
         'name',
         'city',
@@ -16,14 +27,19 @@ class Location extends Model
         'province',
     ];
 
-    public function users(){
+    /**
+     * Relatie: gebruikers die bij deze locatie horen.
+     */
+    public function users()
+    {
         return $this->hasMany(User::class);
-
     }
 
+    /**
+     * Relatie: voorraden van materialen op deze locatie.
+     */
     public function materialStocks()
     {
         return $this->hasMany(MaterialStock::class);
     }
-
 }
