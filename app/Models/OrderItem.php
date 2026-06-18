b7c136b9499efb9822225c6909fcdc316fe8f302
chayma <chayma.abdessadak@student.ehb.be>
2026-06-18 11:53:13 +0200

    Documentatie toegevoegd voor OrderItem model

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -4,22 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * OrderItem Model
+ * 
+ * Vertegenwoordigt een regel binnen een bestelling.
+ * Elke orderitem koppelt een materiaal aan een bestelling met een specifieke hoeveelheid.
+ *
+ * @author 
+ * @version 1.0
+ */
 class OrderItem extends Model
 {
+    /**
+     * Velden die massaal mogen worden ingevuld.
+     */
     protected $fillable = [
-
         'order_id',
         'material_id',
         'quantity'
-
     ];
 
+    /**
+     * Relatie: deze orderitem hoort bij een bestelling.
+     */
     public function order()
     {
         return $this->belongsTo(Order::class);
     }
+
+    /**
+     * Relatie: deze orderitem hoort bij een materiaal.
+     */
     public function material()
-{
-    return $this->belongsTo(Material::class);
-}
+    {
+        return $this->belongsTo(Material::class);
+    }
 }
