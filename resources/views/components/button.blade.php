112fddc606c1a11371fa877ce4552556097ef624
chayma <chayma.abdessadak@student.ehb.be>
2026-06-18 12:32:39 +0200

    Documentatie toegevoegd voor button component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,10 @@
+{{--
+    BUTTON COMPONENT
+    Herbruikbare knop met Aquafin stijl (blauw).
+    @author 
+    @version 1.0
+--}}
+
 <button
     {{ $attributes->merge([
         'class' => 'bg-[#0F4C81] hover:bg-[#1E6BA8] text-white px-5 py-2 rounded-lg transition'
